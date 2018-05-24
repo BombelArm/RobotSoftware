6933740d4ea03c136d6076fb6981c87a34bf1301
michal.rom089 <michal.rom089@gmail.com>
2018-05-24 15:24:16 +0200

    working pyserial

--- serial_communication/src/test.py
+++ serial_communication/src/test.py
@@ -2,6 +2,7 @@
 # license removed for brevity
 import rospy
 import serial
+import struct
 import time
 from bombel_msg import bombel_msg
 from std_msgs.msg import String
@@ -12,25 +13,24 @@ def shutdown():
     ser.close()
     print "\n Shutdown! \n"
 
+
 def serial_send(msg):
     line = []
-
+    if msg > 99:
+        print "Msg is defined as 8bit (int<256)"
+        return
     if ser.isOpen():
 
         try:
             ser.flushInput() #flush input buffer, discarding all its contents
             ser.flushOutput()#flush output buffer, aborting current output 
-            #and discard all that is in buffer
 
-            #write data
-            # bytes_sent = ser.write(bytes(14, 'utf-8'))
-            bytes_sent = ser.write(msg)
-            print(str(bytes_sent) + "write data:" + msg)
+            bytes_sent = ser.write(struct.pack('!B',msg))
 
-            time.sleep(0.1)  #give the serial port sometime to receive the data
-            response = ser.readline().decode('ascii')
-            # response = ser.readline()
-            print("read data: " + response)
+            # time.sleep(0.1)  #give the serial port sometime to receive the data
+            # response = ser.readline().decode('ascii')
+            # # response = ser.readline()
+            # print("read data: " + response)
 
         except Exception, e1:
             print "error communicating...: " + str(e1)
@@ -40,46 +40,74 @@ def serial_send(msg):
 
 def loop():
     while not rospy.is_shutdown():
-        # disp_menu(0)
-        # msg_type = raw_input('Choose option:')
-        disp_menu(1)
-        msg_option = raw_input('Choose option:')
-        # if int(msg_type) == bombel_msg.HW_CONFIG:
-        hw_config_cmd(msg_option)   
-        
-
-
-        # if str1 == 'q':
-        #     rospy.signal_shutdown('Keyboard interrupt')
-        # else:
-        #     serial_send(str1)
-
+        disp_menu(0)
+        option = int(raw_input('Choose option:'))
+        if option == 1:
+            disp_menu(1)            
+            hw_config_cmd()   
+        elif option == 2:
+            disp_menu(2)
+            joint_space_cmd()
+        elif option == 'q':
+            rospy.signal_shutdown('Keyboard interrupt')
 
         rate.sleep()
 
 def disp_menu(option):
     if option == 0:
+        print('======MAIN MENU========')
         print('[1]' + 'Hardware configuration')
         print('[2]' + 'JINT')
         print('[3]' + 'OINT')
+        print('[Q]' + 'Quit')
+
     elif option == 1:
+        print('======HW_CONFIG========')
         print('[1]' + 'Turn motors on')
         print('[2]' + 'Turn motors off')
         print('[3]' + 'Turn fans on')
         print('[4]' + 'Turn fans off')
+    elif option == 2:
+        print('======JOINT_SPACE========')
     else:
         print('Invalid option')
-def hw_config_cmd(cmd_type):
-    order_type=str(bombel_msg.HW_CONFIG).zfill(2)
-    hw_config_cmd=str(cmd_type).zfill(2)
-    offset=str(0);
 
-    print(bytesorder_type[0])
-    # print(msg + '\n')
-    # if len(msg) != bombel_msg.ORDER_LENGTH:
-    #     print("Incorrect message format! \n")
-    # else:
-    #     serial_send(msg)
+def joint_space_cmd():
+    f0=float(raw_input('JOINT 0 :'))
+    f1=float(raw_input('JOINT 1 :'))
+    f2=float(raw_input('JOINT 2 :'))
+
+    if f0 >= 10 or f1 >=10 or f2 >= 10:
+        print("F0 or F1 or F2: Out of range")
+        return
+
+    str0="%1.2f" % f0
+    str1="%1.2f" % f1
+    str2="%1.2f" % f2
+
+    serial_send(10*bombel_msg.JOINT_SPACE+int(str0[0]))
+    serial_send(10*int(str0[2])+int(str0[3]))
+    serial_send(10*int(str1[0])+int(str1[2]))
+    serial_send(10*int(str1[3])+int(str2[0]))
+    serial_send(10*int(str2[2])+int(str2[3]))
+
+def hw_config_cmd():
+    option=int(raw_input('Choose option: '))
+
+    if option == bombel_msg.MOTORS_ON:
+        action = bombel_msg.MOTORS_ON;
+    elif option == bombel_msg.MOTORS_OFF:
+        action = bombel_msg.MOTORS_OFF;
+    elif option == bombel_msg.FANS_ON:
+        action = bombel_msg.FANS_ON;
+    elif option == bombel_msg.FANS_OFF:
+        action = bombel_msg.FANS_OFF;
+
+    serial_send(10*bombel_msg.HW_CONFIG);
+    serial_send(10*(action%10))
+    serial_send(00)
+    serial_send(00)
+    serial_send(00)
 
 def serial_init():
     global ser
@@ -90,7 +118,7 @@ def serial_init():
     #    3. x, x is bigger than 0, float allowed, timeout block call
 
     ser = serial.Serial()
-    ser.port = "/dev/ttyACM0"
+    ser.port = "/dev/ttyACM1"
     ser.baudrate = 115200
     ser.bytesize = serial.EIGHTBITS #number of bits per bytes
     ser.parity = serial.PARITY_NONE #set parity check: no parity
